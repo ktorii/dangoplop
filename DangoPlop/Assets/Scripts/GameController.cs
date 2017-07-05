9d3a10e24c87c98b672ebfd8d5c98d27c6b82328
Ken Torii <ken.torii7@gmail.com>
2017-07-04 20:40:19 -0400

    Add main structure

---
+++ DangoPlop/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+//	public GUIText scoreText;
+
+	// Use this for initialization
+	void Start () {
+//		StartCoroutine (SpawnWaves ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
+		
+	}
+	public void AddScore(int newScoreValue) {
+//		scorer = blah;
+//		UpdateScore();
+	}
+	void UpdateScore() {
+//		scoreText.text = "Score: " + score;
+	}
+//	IEnumerator SpawnWaves() {
+//		yield return new WaitForSeconds (200);
+//	}
+}
