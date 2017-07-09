b995c8d7818a6330f52958ecc0f2010c5544ad39
Ken Torii <ken.torii7@gmail.com>
2017-07-08 23:28:26 -0400

    Add gitignore and base file structure

--- DangoPlop/Assets/Scripts/GameController.cs
+++ DangoPlop/Assets/Scripts/GameController.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-//	public GUIText scoreText;
 
-	// Use this for initialization
 	void Start () {
-//		StartCoroutine (SpawnWaves ());
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.R)) {
 			Application.LoadLevel (Application.loadedLevel);
 		}
-		
 	}
+
 	public void AddScore(int newScoreValue) {
-//		scorer = blah;
-//		UpdateScore();
 	}
+
 	void UpdateScore() {
-//		scoreText.text = "Score: " + score;
 	}
-//	IEnumerator SpawnWaves() {
-//		yield return new WaitForSeconds (200);
-//	}
+
 }
