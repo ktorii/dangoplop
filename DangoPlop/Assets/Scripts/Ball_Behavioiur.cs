ea25f345d85226e653a47fa6aa04f4616f42a477
Ivan Chow <ivanchow2468@gmail.com>
2017-07-10 17:45:04 -0400

    added bouncing ball prefab

---
+++ DangoPlop/Assets/Scripts/Ball_Behavioiur.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Behavioiur : MonoBehaviour {
+    public GameObject player;
+    public GameObject ball;
+    public Vector2 ballPos;
+    public Rigidbody2D rb;
+    public int thrust;
+    public CircleCollider2D circle;
+    public int counter;
+
+    // Use this for initialization
+    void Start () {
+        ball = GameObject.FindGameObjectWithTag("Ball");
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
+        thrust = 100;
+        rb.AddForce(Vector2.right * thrust); 
+
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ballPos = ball.transform.position;
+        circle.isTrigger = false;
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ball")
+        {
+            circle.isTrigger = true;
+            
+        }
+    }
+}
