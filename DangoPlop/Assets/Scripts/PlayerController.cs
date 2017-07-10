1a07d2fd579b25ebf14210557a66ea8098b82f3e
GuangZhi Xiao <gz_xiao@live.com>
2017-07-10 01:47:12 -0400

    Add basic movement system

--- DangoPlop/Assets/Scripts/PlayerController.cs
+++ DangoPlop/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
-	public float speed;
+	public float speedScale;
+	public float maxHorizontalSpeed;
+	public float baseJumpPower;
+	public float groundYPosition;
 
 	void Start() {
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce (movement * speed);
 
+		float moveVertical = Input.GetAxis ("Vertical");
+		// dango can only jump if it's on the ground
+		if(moveVertical > 0 && rb2d.position.y <= groundYPosition){
+			moveVertical = baseJumpPower;
+		}
+		else{
+			moveVertical = 0;
+		}
+
+		Vector2 verticalMoveDelta = new Vector2 (0, moveVertical);
+		rb2d.AddForce (verticalMoveDelta, ForceMode2D.Impulse);
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector2 horizontalMoveDelta = new Vector2 (moveHorizontal, 0);
+
+		rb2d.AddForce (horizontalMoveDelta * speedScale, ForceMode2D.Impulse);
+
+		// limit dango's horizontal speed
+		if(rb2d.velocity.x > maxHorizontalSpeed){
+			rb2d.velocity.Set (maxHorizontalSpeed, rb2d.velocity.y);
+		}
 	}
 
 }
