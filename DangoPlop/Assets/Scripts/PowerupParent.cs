c5c8e2e7ce064a02182e2220e66e60ef911c97aa
GuangZhi Xiao <gz_xiao@live.com>
2017-07-16 14:06:18 -0400

    Add Powerup Parent as Prefab
    Powerups have 2 colliders; 1 is for physics simulation and smaller, the
    other collider is slightly larger and is considered a trigger.
    
    Landing check is based on the Ground Y Position public value, which is
    0.3 like the landing detection for Dango. It's very hacky but the other
    way to do it is using raycasting; performance might not be problem on PC
    but could be issue on mobile.
    
    Powerups also disappear 2 seconds after landing. The duration can be
    changed by the timeLandedToDestroy public value.
    
    To test, move a bunch of PowerupParent prefab into the scene.

---
+++ DangoPlop/Assets/Scripts/PowerupParent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupParent : MonoBehaviour {
+
+	static private string playerTag = "Player";
+	public float timeLandedToDestroy = 3;
+	public float groundYPosition;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.y <= groundYPosition){
+			Destroy(gameObject, timeLandedToDestroy);
+		}
+	}
+
+	// What to do when powerup collides with player
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == playerTag){
+			print("powerup collision detected");
+			Destroy(gameObject);
+		}
+	}
+
+}
