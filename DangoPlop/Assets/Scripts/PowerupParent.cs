514f39aea87e9a6d7bb9ec9edc56b71b2ef8f6f3
GuangZhi Xiao <gz_xiao@live.com>
2017-07-24 02:13:00 -0400

    Force powerup to have constant velocity
    Can't accelerate if speed is assigned to constant value every frame.

--- DangoPlop/Assets/Scripts/PowerupParent.cs
+++ DangoPlop/Assets/Scripts/PowerupParent.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PowerupParent : MonoBehaviour {
 
+	private Rigidbody2D rb2d;
 	static private string playerTag = "Player";
 	public float timeLandedToDestroy = 3;
 	public float groundYPosition;
+	public float verticalSpeed;
 
 	// Use this for initialization
 	void Start () {
-
+		rb2d = GetComponent<Rigidbody2D> ();
 	}
-	
+
+	void FixedUpdate () {
+		rb2d.velocity = new Vector2 (0, -verticalSpeed);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if(gameObject.transform.position.y <= groundYPosition){
